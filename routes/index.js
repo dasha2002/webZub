1fba4e06da05ec91ca220fd3326fa0baa041c1cb
Student <maxgit222@mail.ru>
2022-03-29 11:36:46 +0300

    Midg get date

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const {Team} = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,11 +28,14 @@ router.get('/AddNewMatchap', function(req, res, next) {
 });
 
 
-router.get('/teamsMain', function(req, res, next) {
+router.get('/teamsMain',   async function(req, res, next) {
+  let teams = await Team.findAll();
+  console.log(teams);
   res.render('teamsMain', { });
 });
 
-router.get('/teamDetail', function(req, res, next) {
+router.get('/teamDetail',  function(req, res, next) {
+
   res.render('teamDetail', { });
 });
 
